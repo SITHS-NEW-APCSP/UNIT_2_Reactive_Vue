ec683f5eaff34c2963bcd32d9c2ca3f116f7c3d5
savokin <locovname@gmail.com>
2021-07-22 08:22:40 -0400

    safwr

--- basics/src/views/HomeC/cowabunga.vue
+++ basics/src/views/HomeC/cowabunga.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div>
-      <h1>cowabunga</h1>
+      <h1>cowabunga {{$route.params.cow_val}} asfr</h1>
       <h1>cowabunga</h1>
       <h1>cowabunga</h1>
       <h1>cowabunga</h1>
