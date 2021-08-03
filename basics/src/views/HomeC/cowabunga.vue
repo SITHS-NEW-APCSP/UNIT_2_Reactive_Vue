893a053da08f2d8f9d1864a997d6a1471930badf
savokin <locovname@gmail.com>
2021-08-03 10:53:39 -0400

    mor

--- basics/src/views/HomeC/cowabunga.vue
+++ basics/src/views/HomeC/cowabunga.vue
@@ -1,18 +1,16 @@
-<template>
-  <div>
-    <div>
-      <h1>cowabunga {{$route.params.c}} electric boogaloo</h1>      <!--this is us getting the dynamic route param. we always have acsess to it here, no matter the instance or value-->
-      <h1>cowabunga</h1>
-      <h1>cowabunga</h1>
-      <h1>cowabunga</h1>
-      <h1>cowabunga</h1>
-      <h1>cowabunga</h1>
-      <h1>cowabunga</h1>
-      <h1>cowabunga</h1>
-      <h1>cowabunga</h1>
-      <h1>try changing the end of url to a diff number</h1>
-    </div>
-  </div>
-</template>
-
-
+<template> 
+  <div> 
+    <div> 
+      <h1>cowabunga {{$route.params.c}} electric boogaloo</h1>     <!--this is us getting the dynamic route param. we always have acsess to it here, no matter the instance or value-->
+      <h1>cowabunga</h1>    
+      <h1>cowabunga</h1>      
+      <h1>cowabunga</h1>    
+      <h1>cowabunga</h1>      
+      <h1>cowabunga</h1>      
+      <h1>cowabunga</h1>      
+      <h1>cowabunga</h1>            
+      <h1>cowabunga</h1>   
+      <h1>try changing the end of url to a diff number/value after the /</h1>                   
+    </div>          
+  </div>   
+</template>                 
